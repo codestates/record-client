b213b00d266ca38642d4981b2a5c59e02e9feb4e
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2020-12-31 15:35:23 +0900

    navbar update

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,15 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Search from './components/Search/Search';
 import Post from './components/Post/Post';
+import Navbar from './components/Navbar/Navbar';
 import userSchema from './schema/user';
 import postSchema from './schema/post';
 const App = () => {
   const [userData, setUserData] = useState([]); //cardlist로 렌더되는 데이터(post테이블에서 시간순으로)
   const [postData, setPostData] = useState([]);
   const [isLogin, setIsLogin] = useState(false);
-  const [accessToken, setAccessToken] = useState("") //로그인 요청의 응답으로 받은 토큰 
-  const [myData, setMyData] = useState({data: null})  
+  const [accessToken, setAccessToken] = useState(''); //로그인 요청의 응답으로 받은 토큰
+  const [myData, setMyData] = useState({ data: null });
   //로그인한 사용자의 데이터, 어떤 컴포넌트로 내려줘야 할까?
 
   useEffect(() => {
@@ -21,28 +22,31 @@ const App = () => {
     setPostData(postSchema);
   }, []);
 
-  const inputMyInfo = (userInfo) => { 
+  const inputMyInfo = (userInfo) => {
     setMyData({
-      data: userInfo.data
-    })
-  }
+      data: userInfo.data,
+    });
+  };
   return (
     <>
       <Router>
         <Switch>
           <Route exact path="/">
-            <LandingPage
-              isLogin={isLogin}
-              userData={userData}
-              postData={postData}
-              myData={myData}
-            />
+            <Navbar isLogin={isLogin} myData={myData} />
+            <LandingPage userData={userData} postData={postData} />
           </Route>
           <Route exact path="/login">
-            <Login setIsLogin={setIsLogin} setAccessToken={setAccessToken} inputMyInfo={inputMyInfo} />
+            <Login
+              setIsLogin={setIsLogin}
+              setAccessToken={setAccessToken}
+              inputMyInfo={inputMyInfo}
+            />
           </Route>
           <Route exact path="/register" component={Register} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/search">
+            <Navbar isLogin={isLogin} myData={myData} />
+            <Search />
+          </Route>
           <Route exact path="/write">
             <Post
               userData={userData}
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import styles from '../LandingPage/LandingPage.module.css';
 import Navbar from '../Navbar/Navbar';
 import Cardlist from '../Card/Card_list';
-const LandingPage = ({ isLogin, userData, postData, myData }) => {
+const LandingPage = ({ userData, postData }) => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
-        <Navbar isLogin={isLogin} myData={myData} />
         <Cardlist userData={userData} postData={postData} />
       </div>
     </div>
