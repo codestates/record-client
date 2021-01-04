08411efc81d1a1ee3469411b3a85f340354f548f
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-01-04 17:09:45 +0900

    landing

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from '../LandingPage/LandingPage.module.css';
 /* import Navbar from '../Navbar/Navbar'; */
 import Cardlist from '../Card/Card_list';
 import LogoutModal from '../Modal/LogoutModal';
-const LandingPage = ({ userData, postData, isModalShow, modalOff, clearToken, setLogout, accessToken }) => {
+const LandingPage = ({
+  posts,
+  users,
+  isModalShow,
+  modalOff,
+  clearToken,
+  setLogout,
+  accessToken,
+}) => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
-        <Cardlist userData={userData} postData={postData} />
+        <Cardlist posts={posts} users={users} />
       </div>
-      {isModalShow && <LogoutModal modalOff={modalOff} clearToken={clearToken} setLogout={setLogout} accessToken={accessToken}/>}
+      {isModalShow && (
+        <LogoutModal
+          modalOff={modalOff}
+          clearToken={clearToken}
+          setLogout={setLogout}
+          accessToken={accessToken}
+        />
+      )}
     </div>
   );
 };
