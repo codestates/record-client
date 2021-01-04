ca0512942880eb5fb4b979b30b78a5fade1b1121
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-01-04 16:37:53 +0900

    cardList

--- client/src/components/Card/Card_list.jsx
+++ client/src/components/Card/Card_list.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import CardItem from './Card_item';
 import styles from '../Card/Card_list.module.css';
 const Cardlist = ({ posts, users }) => {
+  console.log(posts);
   return (
     <ul className={styles.cardList}>
-      {posts
-        .slice(0)
-        .reverse()
-        .map((item) => (
-          <CardItem
-            key={item.id}
-            postData={item}
-            userData={users}
-            imageUrl={item.imageUrl}
-          />
-        ))}
+      {posts &&
+        posts
+          .slice(0)
+          .reverse()
+          .map((item) => (
+            <CardItem
+              key={item.id}
+              postData={item}
+              userData={users}
+              imageUrl={item.imageUrl}
+            />
+          ))}
     </ul>
   );
 };
